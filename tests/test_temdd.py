353dd82ac1cbc048ee09147fecb8bee1463dcede
ingolf-lohmann <ingolf.lohmann@web.de>
2026-09-15 15:03:22 +0000

    test(temdd): add fail-closed subject-scope regressions

--- tests/test_temdd.py
+++ tests/test_temdd.py
@@ -2,16 +2,16 @@ import unittest
 from tools.qikvrt_temdd import parse
 GOOD='''temdd 0.1; authority owner = "Goldkelch/qik-vrt"; subject s { repository = "Goldkelch/qik-vrt"; binding = exact; } request r { target = QIKVRT_DOD; } on event { follow exact; classify causal; } on blocker { learn smallest_sound_successor; execute successor; } until { ZERO_BUGS && FRESH_EFFECT_READBACK; }'''
 class TEMDDTests(unittest.TestCase):
- def test_deterministic_ir(self):
-  self.assertEqual(parse(GOOD),parse(GOOD))
- def test_exact_binding(self):
-  self.assertEqual(parse(GOOD)['subject']['binding'],'exact')
- def test_dod_conjunction(self):
-  self.assertEqual(parse(GOOD)['dod'],['ZERO_BUGS','FRESH_EFFECT_READBACK'])
+ def test_deterministic_ir(self): self.assertEqual(parse(GOOD),parse(GOOD))
+ def test_exact_binding(self): self.assertEqual(parse(GOOD)['subject']['binding'],'exact')
+ def test_dod_conjunction(self): self.assertEqual(parse(GOOD)['dod'],['ZERO_BUGS','FRESH_EFFECT_READBACK'])
  def test_missing_authority_blocks(self):
   with self.assertRaises(ValueError): parse(GOOD.replace('authority owner = "Goldkelch/qik-vrt";',''))
  def test_nonexact_binding_blocks(self):
   with self.assertRaises(ValueError): parse(GOOD.replace('binding = exact','binding = floating'))
- def test_missing_readback_can_be_detected(self):
-  self.assertNotIn('FRESH_EFFECT_READBACK',parse(GOOD.replace(' && FRESH_EFFECT_READBACK',''))['dod'])
+ def test_missing_repository_blocks(self):
+  with self.assertRaises(ValueError): parse(GOOD.replace('repository = "Goldkelch/qik-vrt";',''))
+ def test_subject_scope_not_global(self):
+  bad=GOOD.replace('repository = "Goldkelch/qik-vrt";','')+' repository = "evil/global";'
+  with self.assertRaises(ValueError): parse(bad)
 if __name__=='__main__': unittest.main()
